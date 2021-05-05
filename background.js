e40b91827ec6bbe17f3b134020570238466db05a
Duncan Harvey <duncan.harvey@datadoghq.com>
2021-05-05 11:36:28 -0400

    only resets views on install if none exist

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
 // on extension installation initialize local view storage, get views from open Zendesk tabs, and listen for view updates
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ views: {} }); // TODO: look to see if there are existing rules?
+  chrome.storage.local.get(["views"], value => {
+    if (Object.keys(value.views).length == 0) {
+      chrome.storage.local.set({ views: {} }); // create views object if none exists
+    }
+  });
 
   chrome.tabs.query({ url: "https://datadog.zendesk.com/agent/*" }, tabs => {
     tabs.forEach(tab => {
